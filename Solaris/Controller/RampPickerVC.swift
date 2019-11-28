8040d5e1fcb31af7d64d09715c6634ee776b7dc8
nazar petruk <nazarpetruk15@gmail.com>
2019-11-28 14:28:26 +0100

    - rotate action added

--- Solaris/Controller/RampPickerVC.swift
+++ Solaris/Controller/RampPickerVC.swift
@@ -30,6 +30,7 @@ class RampPickerVC: UIViewController {
         sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         view.insertSubview(sceneView, at: 0)
         
+        preferredContentSize = size
         let scene = SCNScene(named: "art.scnassets/PopOverScene.scn")!
         sceneView.scene = scene
         
@@ -37,12 +38,27 @@ class RampPickerVC: UIViewController {
         camera.usesOrthographicProjection = true
         scene.rootNode.camera = camera
         
-        let obj = SCNScene(named: "art.scnassets/pipe.dae")
-        let node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
-        node?.scale = SCNVector3Make(0.0012, 0.0012, 0.0012)
-        node?.position = SCNVector3Make(-0.95, 0.5, -1)
+        let rotate = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(0.01 * Double.pi), z: 0, duration: 0.1))
+        
+        var obj = SCNScene(named: "art.scnassets/pipe.dae")
+        var node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
+        node?.runAction(rotate)
+        node?.scale = SCNVector3Make(0.0022, 0.0022, 0.0022)
+        node?.position = SCNVector3Make(1, 0.5, -1)
         scene.rootNode.addChildNode(node!)
         
-        preferredContentSize = size
+        obj = SCNScene(named: "art.scnassets/pyramid.dae")
+        node = obj?.rootNode.childNode(withName: "pyramid", recursively: true)!
+        node?.runAction(rotate)
+        node?.scale = SCNVector3Make(0.0050, 0.0050, 0.0050)
+        node?.position = SCNVector3Make(1, -0.5, -1)
+        scene.rootNode.addChildNode(node!)
+        
+        obj = SCNScene(named: "art.scnassets/quarter.dae")
+        node = obj?.rootNode.childNode(withName: "quarter", recursively: true)!
+        node?.runAction(rotate)
+        node?.scale = SCNVector3Make(0.0078, 0.0078, 0.0078)
+        node?.position = SCNVector3Make(1, -2, -1)
+        scene.rootNode.addChildNode(node!)
     }
 }
